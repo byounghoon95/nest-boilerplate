06ba2aaa1453b44fdae8a14971d127dfd27fefce
owner <toltolyibh@gmail.com>
2024-12-12 18:28:02 +0900

    TypeORM 설정
    1. npm install @nestjs/typeorm typeorm pg
    2. app.module.ts 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'test',
+      password: process.env.DB_PASSWORD || 'test',
+      database: process.env.DB_DATABASE || 'testdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
