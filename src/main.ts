c9a983807efbf17236c525b70de90218b222515e
owner <toltolyibh@gmail.com>
2024-12-12 18:07:09 +0900

    swagger 설정
    1. npm install --save @nestjs/swagger
    2. main.ts에 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('nest boilerplate')
+    .setDescription('nest boilerplate practice')
+    .setVersion('1.0')
+    .build();
+
+  const docs = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, docs);
+
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
